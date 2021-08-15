b4272b4e93aa6045726f536f6d9bb3f714299dad
HatemXhezairi <hatem@motomtech.com>
2021-08-15 14:46:57 +0200

    link PetTable with FoundPetTable

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,4 +3,5 @@ class Pet < ApplicationRecord
   validates :image, blob: { content_type: :image, size_range: 1..5.megabyte}
 
   validates_presence_of  :name, :description, :species, :breed, :lost_on, :last_known_latitude, :last_known_longitude
+  has_many :pets
 end
