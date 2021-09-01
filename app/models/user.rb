094d00d1e753adcfa8e75f0a514a60205f54159c
Hatemii <hatem.xhezairi96@gmail.com>
2021-09-01 22:25:50 +0200

    more styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name, :username, :password_confirmation
+  validates_uniqueness_of :username, :email       
 end
