91f8f1d7712696890aaddfba98569fd91021ecab
Hatemii <hatem.xhezairi96@gmail.com>
2021-09-06 13:21:38 +0200

    fix delete button

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -58,6 +58,7 @@ class PetsController < ApplicationController
 
   # DELETE /pets/1 or /pets/1.json
   def destroy
+    @pet = Pet.find(params[:id])
     @pet.destroy
     respond_to do |format|
       format.html { redirect_to pets_url, notice: "Pet was successfully destroyed." }
