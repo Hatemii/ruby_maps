f0994623581c0d4f092118736cedccb8dd58f6e9
Hatemii <hatem.xhezairi96@gmail.com>
2021-09-10 01:30:29 +0200

    fix delete button

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :pets do
     delete 'remove_image', to: 'pets#remove_image'
   end
+  delete 'pets/:id/delete' => 'pets#destroy', as: 'pets_delete'
+  get '/pets/:id/delete' => 'pets#destroy'
 
   root "home#index"
   get  'about/index', to:'about#index'
