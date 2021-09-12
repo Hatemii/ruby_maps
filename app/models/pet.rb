538cb7658c79299daad243cd754c460e90275b9d
Hatemii <hatem.xhezairi96@gmail.com>
2021-09-12 21:39:07 +0200

    validation only on create

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,11 +2,11 @@ class Pet < ApplicationRecord
   has_one_attached :image
   validates :image, blob: { content_type: :image, size_range: 1..5.megabyte}
 
-  validates_presence_of  :name, :description, :species, :breed, :lost_on, :last_known_latitude, :last_known_longitude
+  validates_presence_of  :name, :description, :species, :breed, :lost_on, :last_known_latitude, :last_known_longitude, on: :create
   has_many :pets
 
   def lost_on_format
-    self.lost_on.strftime('%a, %d %b %Y')
+    self.lost_on.strftime('%a, %d %b %Y') if self.lost_on.present?
   end
 
 end
