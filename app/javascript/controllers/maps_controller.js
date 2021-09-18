4f34c50dbafb3b57068380877e766a5a1051fc0e
Hatemii <hatem.xhezairi96@gmail.com>
2021-09-18 20:12:14 +0200

    fixing autocomplete()

--- app/javascript/controllers/maps_controller.js
+++ app/javascript/controllers/maps_controller.js
@@ -13,56 +13,52 @@ export default class extends Controller {
      this.map = new google.maps.Map(this.mapTarget, {
       center: new google.maps.LatLng(this.data.get("latitude") || 39.5, this.data.get("longitude") || -98.35),
       zoom: 8
-    })
-    
-    this.autocomplete = new google.maps.places.Autocomplete(this.fieldTarget)
-    this.autocomplete.bindTo('bounds', this.map)
-    this.autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
-    this.atocomplete.addListener('place_changed', this.placeChanged.bind(this))
-    
-    this.marker = new google.maps.Marker({
-      position: this.map,
+     })
+     
+    this.autocomplete_handle()
+  }
+
+  autocomplete_handle() {
+    const autocomplete = new google.maps.places.Autocomplete(this.fieldTarget)
+    console.log("this.map: ", this.map)
+
+    autocomplete.bindTo('bounds', this.map)
+    autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
+
+    const marker = new google.maps.Marker({
       map: this.map,
-      draggable: true,
       anchorPoint: new google.maps.Point(0, -29),
       animation: google.maps.Animation.DROP
+    });
+
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace()
+      marker.setVisible(false)
+
+      if (!place.geometry) {
+        window.alert(`No details available for input: ${place.name}`)
+        return
+      }
+
+      if (place.geometry.viewport) {
+        this.map.fitBounds(place.geometry.viewport)
+      
+      } else {
+        this.map.setCenter(place.geometry.location)
+        this.map.setZoom(8)
+      }
+
+      marker.setPosition(place.geometry.location)
+      marker.setVisible(true)
+
+      this.latitudeTarget.value = place.geometry.location.lat()
+      this.longitudeTarget.value = place.geometry.location.lng()
     })
-
   }
 
-
-  placeChanged() {
-    let place = this.autocomplete.getPlace()
-
-    if (!place.geometry) {
-      window.alert(`No details available for input: ${place.name}`)
-      return
-    }
-
-    if (place.geometry.viewport) {
-      this.map.fitBounds(place.geometry.viewport)
-     
-    } else {
-      this.map.setCenter(place.geometry.location)
-      this.map.setZoom(8)
-    }
-
-    this.marker.setPosition(place.geometry.location)
-    this.marker.setVisible(true)
-
-    this.latitudeTarget.value = place.geometry.location.lat()
-    this.longitudeTarget.value = place.geometry.location.lng()
-  }
-
-  keydown(event) {
-    if (event.key == "Enter") {
-      event.preventDefault()
-    }
-  }
-
-  // preventSubmit(e) {
-  //   if (e.key == "Enter") {
-  //     e.preventDefault()
+  // keydown(event) {
+  //   if (event.key == "Enter") {
+  //     event.preventDefault()
   //   }
   // }
 }
