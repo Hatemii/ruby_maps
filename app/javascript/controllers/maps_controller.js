152688021a6de32eb0e66fe895c3b2ac8272c5cc
Hatemii <hatem.xhezairi96@gmail.com>
2021-09-18 19:52:31 +0200

    autocomplete()

--- app/javascript/controllers/maps_controller.js
+++ app/javascript/controllers/maps_controller.js
@@ -10,37 +10,33 @@ export default class extends Controller {
   }
 
   initMap() {
-    this.map = new google.maps.Map(this.mapTarget, {
-      center: new google.maps.LatLng(
-        // this.latitudeTarget.value,
-        // this.longitudeTarget.value
-        39.5,
-        98.35
-      ),
+     this.map = new google.maps.Map(this.mapTarget, {
+      center: new google.maps.LatLng(this.data.get("latitude") || 39.5, this.data.get("longitude") || -98.35),
       zoom: 8
     })
-
-    this.marker = new google.maps.Marker({
-      position: { lat: 39.5, lng: 98.35 },
-      map: this.map,
-      title: "City Name",
-      draggable: true,
-      animation: google.maps.Animation.DROP
-    })
-
+    
     this.autocomplete = new google.maps.places.Autocomplete(this.fieldTarget)
     this.autocomplete.bindTo('bounds', this.map)
     this.autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
-    this.autocomplete.addListener('place_changed', this.placeChanged.bind(this))
+    this.atocomplete.addListener('place_changed', this.placeChanged.bind(this))
+    
+    this.marker = new google.maps.Marker({
+      position: this.map,
+      map: this.map,
+      draggable: true,
+      anchorPoint: new google.maps.Point(0, -29),
+      animation: google.maps.Animation.DROP
+    })
+
   }
 
 
   placeChanged() {
-    let place = this.autocomplete().getPlace()
+    let place = this.autocomplete.getPlace()
 
     if (!place.geometry) {
-      window.alert(`No details available for input: ${place.name}`);
-      return;
+      window.alert(`No details available for input: ${place.name}`)
+      return
     }
 
     if (place.geometry.viewport) {
@@ -58,8 +54,15 @@ export default class extends Controller {
     this.longitudeTarget.value = place.geometry.location.lng()
   }
 
-  // preventSubmit(e) {
-  //   if (e.key == "Enter") { e.preventDefault() }
-  // }
+  keydown(event) {
+    if (event.key == "Enter") {
+      event.preventDefault()
+    }
+  }
 
+  // preventSubmit(e) {
+  //   if (e.key == "Enter") {
+  //     e.preventDefault()
+  //   }
+  // }
 }
