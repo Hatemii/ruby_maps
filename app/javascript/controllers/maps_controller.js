c89283436a893983ee88fab10be62770ba277428
HatemXhezairi <hatem@motomtech.com>
2021-11-21 22:35:54 +0100

    distance calculator

--- app/javascript/controllers/maps_controller.js
+++ app/javascript/controllers/maps_controller.js
@@ -1,4 +1,4 @@
-import { Controller} from "stimulus"
+import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = ["field", "map", "latitude", "longitude"]
@@ -13,27 +13,10 @@ export default class extends Controller {
     this.map = new google.maps.Map(this.mapTarget, {
       center: new google.maps.LatLng(this.data.get("latitude") || 1.290270, this.data.get("longitude") || 103.851959),
       zoom: 12,
-      minZoom:8
+      minZoom: 8
     })
 
-    this.mouseClickHandle()
     this.autocomplete_handle()
-  }
-
-  mouseClickHandle() {
-    const marker = new google.maps.Marker({
-        map: this.map,
-        animation: google.maps.Animation.DROP,
-        visible: false
-     });
-    
-    this.map.addListener('click', event => {
-      this.latitudeTarget.value = event.latLng.lat().toFixed(5)
-      this.longitudeTarget.value = event.latLng.lng().toFixed(5)
-
-      marker.setPosition(event.latLng)
-      marker.setVisible(true)
-    });
   }
 
   autocomplete_handle() {
@@ -59,7 +42,7 @@ export default class extends Controller {
 
       if (place.geometry.viewport) {
         this.map.fitBounds(place.geometry.viewport)
-      
+
       } else {
         this.map.setCenter(place.geometry.location)
         this.map.setZoom(8)
@@ -68,14 +51,48 @@ export default class extends Controller {
       marker.setPosition(place.geometry.location)
       marker.setVisible(true)
 
-      this.latitudeTarget.value = place.geometry.location.lat()
-      this.longitudeTarget.value = place.geometry.location.lng()
+      this.latitudeTarget.value = place.geometry.location.lat().toFixed(6)
+      this.longitudeTarget.value = place.geometry.location.lng().toFixed(6)
+
+      this.mouseClickHandle(this.latitudeTarget.value, this.longitudeTarget.value)
     })
+
   }
 
-  // keydown(event) {
-  //   if (event.key == "Enter") {
-  //     event.preventDefault()
-  //   }
-  // }
+  mouseClickHandle(search_lat, search_lng) {
+    const marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      visible: false
+    });
+
+    var current_base_lat_lng = new google.maps.LatLng(search_lat, search_lng)
+
+    this.map.addListener('click', event => {
+      var mouse_lat_lng = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng())
+
+      if (this.distance_calculate(current_base_lat_lng, mouse_lat_lng)) {
+        this.latitudeTarget.value = this.latitudeTarget.value
+        this.longitudeTarget.value = this.longitudeTarget.value
+
+      } else {
+        this.latitudeTarget.value = event.latLng.lat().toFixed(6)
+        this.longitudeTarget.value = event.latLng.lng().toFixed(6)
+
+        marker.setPosition(event.latLng)
+        marker.setVisible(true)
+      }
+    });
+  }
+
+  distance_calculate(point_1, point_2) {
+    var distance = (google.maps.geometry.spherical.computeDistanceBetween(point_1, point_2) / 1000).toFixed(1);
+    const round_distance = Math.round(distance)
+
+    if (round_distance > 10) {
+      alert("Distance more than 10km from your base location is invalid!");
+      return true
+    }
+    return false
+  }
 }
