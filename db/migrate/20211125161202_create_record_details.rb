6b84869bf5885cc6a76973120c3c056bfb167d27
HatemXhezairi <hatem@motomtech.com>
2021-11-28 22:55:21 +0100

    default value added to location_details model

--- db/migrate/20211125161202_create_record_details.rb
+++ db/migrate/20211125161202_create_record_details.rb
@@ -1,11 +1,11 @@
 class CreateRecordDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :record_details do |t|
-      t.integer :street_number
-      t.string :route
-      t.string :neighborhood
-      t.string :locality
-      t.string :country
+      t.string :street_number, :default =>  "not-available"
+      t.string :route, :default =>  "not-available"
+      t.string :neighborhood, :default =>  "not-available"
+      t.string :locality, :default =>  "not-available"
+      t.string :country, :default =>  "not-available"
       t.integer :postal_code
       t.references :record_detailable, polymorphic: true, null: false
 
--- db/migrate/20211127170520_change_column_types.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnTypes < ActiveRecord::Migration[6.1]
-  def change
-    change_column :record_details, :street_number, :string
-  end
-end
